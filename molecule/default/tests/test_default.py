6c37b1cffd5b70004105d5d0e78b5376f8491491
Bodo Schulz <bodo@boone-schulz.de>
2021-08-24 13:31:51 +0200

    fixes (again)

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -31,17 +31,30 @@ def get_vars(host):
 
     """
     base_dir, molecule_dir = base_directory()
+    distribution = host.system_info.distribution
 
-    file_defaults = "file={}/defaults/main.yml name=role_defaults".format(base_dir)
-    file_vars = "file={}/vars/main.yml name=role_vars".format(base_dir)
-    file_molecule = "file={}/group_vars/all/vars.yml name=test_vars".format(molecule_dir)
+    if distribution in ['debian', 'ubuntu']:
+        os = "debian"
+    elif distribution in ['centos', 'redhat', 'ol']:
+        os = "redhat"
+    elif distribution in ['arch']:
+        os = "archlinux"
+
+    print(" -> {} / {}".format(distribution, os))
+
+    file_defaults = "file={}/defaults/main.yaml name=role_defaults".format(base_dir)
+    file_vars = "file={}/vars/main.yaml name=role_vars".format(base_dir)
+    file_molecule = "file={}/group_vars/all/vars.yaml name=test_vars".format(molecule_dir)
+    file_distibution = "file={}/vars/{}.yaml name=role_distibution".format(base_dir, os)
 
     defaults_vars = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
     vars_vars = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
+    distibution_vars = host.ansible("include_vars", file_distibution).get("ansible_facts").get("role_distibution")
     molecule_vars = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
 
     ansible_vars = defaults_vars
     ansible_vars.update(vars_vars)
+    ansible_vars.update(distibution_vars)
     ansible_vars.update(molecule_vars)
 
     templar = Templar(loader=DataLoader(), variables=ansible_vars)
@@ -51,8 +64,10 @@ def get_vars(host):
 
 
 def test_directory(host, get_vars):
+
+    # pp.pprint(get_vars)
+
     dir = host.file(get_vars.get('harbor_datadir'))
-    assert dir.exists
     assert dir.is_directory
 
 
@@ -62,5 +77,4 @@ def test_directory(host, get_vars):
 def test_files(host, get_vars, files):
     dir = host.file(get_vars.get('harbor_install_dir'))
     f = host.file("{0}/{1}".format(dir.linked_to, files))
-    assert f.exists
     assert f.is_file
